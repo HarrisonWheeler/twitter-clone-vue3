7a1a454639245db67ec64df8c7ddfc89751d9ace
Harrison Wheeler <wheelerharrison93@gmail.com>
2020-10-21 11:12:41 -0600

    basic lay out - other small changes

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
-  <div id="nav"></div>
-  <router-view />
+  <div class="app">
+    <router-view />
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+export default {
+  name: "App",
+  props: {},
+};
+</script>
+<style lang="sass">
+@import "~bootswatch/dist/flatly/variables"
+@import "~bootstrap/scss/bootstrap"
+@import "~bootswatch/dist/flatly/bootswatch"
+</style>;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home container-fluid bg-primary bg-full">
+    <div class="row">
+      <div class="col-2"></div>
+      <div class="col-6"></div>
+      <div class="col-4">
+        <div class="form-group p-2">
+          <input
+            type="text"
+            class="form-control search-bar"
+            placeholder="Search Twitter Clone"
+            id="inputDefault"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,8 +23,14 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
+<style scoped>
+.bg-full {
+  min-height: 101%;
+}
+.search-bar {
+  border-radius: 20px 20px 20px 20px;
+}
+</style>
